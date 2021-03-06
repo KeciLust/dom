bfe777c06a7901c0dbe198860d37018dbde70ea0
Denis Nikitin <dinya.nikitin@gmail.com>
2021-03-06 17:12:10 +0300

    1 change Finish

--- ads/task.js
+++ ads/task.js
@@ -2,25 +2,23 @@
 const rotator = document.querySelector(`.rotator`);
 
 function Rotator(rotator) {
-    
     let i = 0;
-    let timer = Number(rotator.children[i].dataset.speed);
-    setInterval(() => {
+    function timerRotator() {
         if (i < rotator.children.length - 1) {
             rotator.children[i].classList.remove(`rotator__case_active`);
             i++;
             rotator.children[i].classList.add(`rotator__case_active`);
-            
+
         } else {
             rotator.children[i].classList.remove(`rotator__case_active`);
             i = 0;
             rotator.children[i].classList.add(`rotator__case_active`);
         }
-        rotator.children[i].style.color = rotator.children[i].dataset.color; 
-        timer = Number(rotator.children[i].dataset.speed);
-    
-        
-    }, timer);
+        rotator.children[i].style.color = rotator.children[i].dataset.color;
+        const timer = rotator.children[i].dataset.speed;
+        setTimeout(timerRotator, timer);
+    }
+    timerRotator();
 }
 
 Rotator(rotator);
